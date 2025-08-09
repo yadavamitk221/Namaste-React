89ca4b2dbf7791a7d7f55a73930cba60ab6f0912
Amitkumar Yadav <yadavamitk221@gmail.com>
2025-08-09 18:55:51 +0530

    added counter component

--- index.js
+++ index.js
@@ -1,54 +1,20 @@
+const obj = { a: 1, b: { c: 2 } };
+const shallowCopy = { ...obj };
 
-const calculateAge = () => {
-    let selectedBirthDate = "2001-04-22";
-    const todaysDate = new Date(), birth = new Date(selectedBirthDate);
-    if (birth > todaysDate) return console.error("Birthdate cannot be in the future");
+shallowCopy.a = 10;  // Changes only in the copy
+shallowCopy.b.c = 20; // Changes in both copy and original
 
-    let year = todaysDate.getFullYear() - birth.getFullYear();
-    let months = todaysDate.getMonth() - birth.getMonth();
-    let days = todaysDate.getDate() - birth.getDate();
+console.log(obj.b.c); // 20 (Mutated)
+console.log(shallowCopy.b.c); // 20
 
-    if (days < 0){
-        days += new Date(todaysDate.getFullYear(), todaysDate.getMonth(), 0).getDate();
-        months--;
-    }
-    if (months < 0) {
-        months += 12;
-        year--;
-    }
+console.log(shallowCopy.a); // 20
 
-}
+let arr = [1,2,3,4,5];
 
+console.log("arr",arr =  JSON.stringify(arr));
 
-calculateAge();
+console.log(arr[1]); // 20 (Mutated)
 
 
 
 
-let calcumateAgeRevise = (dob) =>{
-    let dateOfBirth = new Date(dob); 
-
-    let today = new Date(); 
-
-    let year = dateOfBirth.getFullYear() - today.getFullYear();
-    let month = today.getFullMonth() = dateOfBirth.getFullMonth(); 
-    let date = today.getDate() - dateOfBirth.getDate(); 
-
-
-    
-if(data < 0){
-    let previousMonth = newDate(today.getFullYear, today.getFullMonth(), 0).getDate();
-    date += previousMonth;
-    month--;
-}
-
-if(Month < 0){
-    month += 12; 
-    year--;
-}
-
-}
-
-
-
-return {year, month, date}
