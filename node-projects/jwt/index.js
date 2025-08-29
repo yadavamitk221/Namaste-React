e1e97cf4acfc58087a7385a300738c626f8e2eaa
Amitkumar Yadav <yadavamitk221@gmail.com>
2025-08-29 22:24:40 +0530

    set-up the jwt folder structure

---
+++ node-projects/jwt/index.js
@@ -0,0 +1,19 @@
+const express = require("express"); 
+const jwt = require("jsonwebtoken"); 
+const app = express(); 
+
+app.use(express.json()); 
+
+app.get("/login", (req, res) => {
+    console.log("req.body", req.body);
+    if(req.body){
+        res.send("hey you are logged in:", req.body);
+    }
+})
+
+app.listen(5000, (err) => {
+    if(err){
+        return  console.log("error while settingup the server", err);
+    }
+    console.log("Server is up and running on port 5000");
+})
